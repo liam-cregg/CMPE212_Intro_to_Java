e9762c24c625310072e44219b0a54c8907bc4841
liam-cregg <liamcregg99@gmail.com>
2019-01-30 22:06:15 -0500

    Completed game. Could still add methods to replay the game, if desired.

--- Assignment1/src/Assignment1.java
+++ Assignment1/src/Assignment1.java
@@ -3,95 +3,177 @@
  * @author Liam - 16LBC1
  */
 
-import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
 /**
  * Plays the game of Pig against the computer.
+ * When starting, must click on the line below the initial prompt before hitting enter.
+ * Then play the game only using the keyboard, if you move the cursor it will affect the input buffer.
  */
 public class Assignment1 {
 
     static Random GENERATOR = new Random(System.currentTimeMillis());
     static final Scanner SCREEN = new Scanner(System.in);
-    static final String[] DICE_ARRAY = {"one", "two", "three", "four", "five", "six"};
-    static final String[] CONDITIONS = {"DOUBLE ONES", "DOUBLES", "SINGLE ONE :("};
+    static final String[] DICE_ARRAY = {"", "one", "two", "three", "four", "five", "six"};
+    static final String[] CONDITIONS = {"DOUBLE ONES :) Must roll again!", "DOUBLES :) Must roll again!", "SINGLE ONE :( Turn sum is zero!"};
 
     public static void main(String[] args) {
         int playerScore = 0;
         int computerScore = 0;
-        System.out.println("Welcome to Pig! You will start");
-        //TODO Integrate methods
-
+        boolean playersTurn = true;
+        System.out.println("Welcome to Pig! You go first, press <enter> to start:");
+        SCREEN.nextLine();
+        for (int i = 0; playerScore < 100 && computerScore < 100; i++) {
+            if(playersTurn) {
+                playerScore = playTurn(playerScore, "Human");
+                playersTurn = false;
+            }
+            else {
+                computerScore = playTurn(computerScore, "Computer");
+                playersTurn = true;
+            }
+            System.out.print(
+                    "\nPlayer's sum is: " + playerScore +
+                    " , Computer's sum is " + computerScore);
+            // ask for confirmation if game not over
+            if(playersTurn && playerScore < 100 && computerScore < 100) {
+                System.out.println(". Press <enter> to start round " + (i + 1));
+                SCREEN.nextLine();
+            }
+            else
+                System.out.println();
+        }
+        if(playerScore >= 100)
+            System.out.println("Human wins!");
+        else
+            System.out.println("Computer wins!");
     }
 
     /**
      * Get's the user input, allowing 'y' or 'n'
      * @return the string entered by the user
      */
-    public static String getInput() {
+    public static boolean getInput() {
         String userString = null;
-        boolean inputOK = false;
-        while (!inputOK) {
-            System.out.print("Enter y or n: ");
+        while (true) {
+            System.out.print("Roll again? Enter y or n: ");
             userString = SCREEN.nextLine();
             // check if valid input
-            if(userString.equals("y") || userString.equals("n"))
-                inputOK = true;
-            else
-                System.out.println("\"" + userString + "\" is not a legal input, please enter 'y' or 'n'!");
-        } // end input loop
-        return userString;
-    }
-
-    public static int rollDice() {
-        return GENERATOR.nextInt(6);
-    }
-
-    public static int playTurn(int currentScore, boolean humanTurn) {
-        int[] diceValues = new int[2];
-        int condition;
-        if(humanTurn) {
-            System.out.println("Human's turn:");
-            do {
-                // store dice rolls in array
-                diceValues[0] = rollDice();
-                diceValues[1] = rollDice();
-                System.out.println("You rolled " + DICE_ARRAY[diceValues[0]] + " and " + DICE_ARRAY[diceValues[1]]);
-                condition = checkSpecial(diceValues);
-                // check if regular roll
-                if(condition == -1) {
-                    //TODO Update values, ask if go again
-                }
-                if{CONDITIONS[condition].equals("DOUBLE ONES")
-                    //TODO Make go again
-                }
-                if(CONDITIONS[condition].equals("DOUBLES")) {
-                    //TODO Make go again
-                }
-                if(CONDITIONS[condition].equals("SINGLE ONE :("){
-                    //TODO Make exit
-                }
+            switch(userString) {
+                case "y":
+                case "Y":
+                    return true;
+                case "n":
+                case "N":
+                    return false;
+                default:
+                    System.out.println("\"" + userString + "\" is not a legal input, please enter 'y' or 'n'!");
             }
-        }
+        } // end input loop
     }
 
-    public static int checkSpecial(int[] diceValues) {
+    /**
+     * The computer must decide whether to continue playing the game.
+     * It's strategy is to stop playing if it's turn score is high enough to win, or over 40.
+     * @param currentScore  the total score of the computer
+     * @param turnScore     the turn score of the user
+     * @return              a boolean value, true if computer decides to replay
+     */
+    public static boolean getComputerInput(int currentScore, int turnScore) {
+        return !((currentScore + turnScore >= 100) || turnScore > 20);
+    }
+
+    /**
+     * Determines if there are any special game conditions met by the dice roll.
+     * Double ones  -> add 25 to turn score
+     * Doubles      -> double value of dice and add to turn score
+     * Single one   -> identified by -1, code handled in playTurn method
+     * Regular roll -> identified by 0, code handles in playTurn method
+     * @param diceValues    the array of rolled values
+     * @param turnScore     the current turn score
+     * @return              the additional score, or the 0/-1 identifier
+     */
+    public static int checkSpecial(int[] diceValues, int turnScore) {
         // check if doubles
-        if(diceValues[0] == diceValues[1]) {
+        if (diceValues[0] == diceValues[1]) {
             // check if double ones (first element in conditions list)
-            if(diceValues[0] == 1) {
-                return 0;
+            if (diceValues[0] == 1) {
+                System.out.println(CONDITIONS[0]);
+                return 25;
             }
             // must be regular doubles (second element in conditions list)
-            else
-                return 1;
+            else {
+                System.out.println(CONDITIONS[1]);
+                return (2 * (diceValues[0] + diceValues[1]));
+            }
         }
         // check if single one (third element in conditions list)
-        else if (diceValues[0] == 1 || diceValues[1] == 1)
-            return 2;
-        // must be regular roll (not a special condition)
-        else
+        else if (diceValues[0] == 1 || diceValues[1] == 1) {
+            System.out.println(CONDITIONS[2]);
             return -1;
         }
+        // must be regular roll (not a special condition)
+        else
+            return 0;
     }
+
+    /**
+     * Randomly rolls dice and returns an array with 2 integers, each between 1 and 6
+     * @return an array of integers, each corresponding to an index of DICE_ARRAY
+     */
+    public static int[] rollDice(int[] diceValues) {
+        diceValues[0] = GENERATOR.nextInt(6) + 1;
+        diceValues[1] = GENERATOR.nextInt(6) + 1;
+        return diceValues;
+    }
+
+    /**
+     * Plays one turn of the game, checking if there are any special conditions met and adjusting score accordingly.
+     * @param currentScore  the current score of the player (human or computer)
+     * @param player        the name of the player (human or computer) as a String
+     * @return              the new total score of the player (human or computer)
+     */
+    public static int playTurn(int currentScore, String player) {
+        int[] diceValues = new int[2];
+        int condition;
+        int turnScore = 0;
+        boolean replay = true;
+        System.out.println(player + "\'s turn:");
+        do {
+            // store dice rolls in array
+            diceValues = rollDice(diceValues);
+            System.out.println(player + " rolled " + DICE_ARRAY[diceValues[0]] +
+                               " and " + DICE_ARRAY[diceValues[1]]);
+            condition = checkSpecial(diceValues, turnScore);
+            // if regular roll
+            if (condition == 0) {
+                turnScore += diceValues[0] + diceValues[1];
+                System.out.println(player + "'s turn sum is: " + turnScore +
+                                   " and game sum would be: " + (currentScore + turnScore));
+                if (player.equals("Human")) {
+                    replay = getInput();
+                }
+                else {
+                    replay = getComputerInput(currentScore, turnScore);
+                }
+            }
+            // if single one
+            else if (condition == -1) {
+                replay = false;
+                turnScore = 0;
+            }
+            // else must be doubles or double ones, must go again
+            // condition = bonus score
+            else {
+                turnScore += condition;
+                System.out.println(player + "'s turn sum is: " + turnScore +
+                                   " and game sum would be: " + (currentScore + turnScore));
+            }
+        } while (replay); // end do-while loop
+
+        System.out.println("Turn over");
+        return currentScore + turnScore;
+    }
+}
+
